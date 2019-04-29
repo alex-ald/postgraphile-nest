3fd4db32e335b1358c39b8173ddf8f10df3fbd6c
Alex Aldana <alexandern.aldana@gmail.com>
2019-04-28 23:17:57 -0400

    Updated SchemaType parameters
    - Decorator now takes a single object, just as the other decorators do, to specify options

--- lib/decorators/schema-type.decorator.ts
+++ lib/decorators/schema-type.decorator.ts
@@ -1,14 +1,15 @@
 import { SetMetadata } from '@nestjs/common';
 import { SCHEMA_TYPE_METADATA } from '../postgraphile.constants';
+import { SchemaTypeOptions } from '../interfaces/schema-type-options.interface';
 
-export function SchemaType(typeName: string) {
+export function SchemaType(options: SchemaTypeOptions) {
   return (
     // tslint:disable-next-line:ban-types
     target: object | Function,
     key?: string | symbol,
     descriptor?: any,
   ) => {
-    SetMetadata(SCHEMA_TYPE_METADATA, { typeName })(
+    SetMetadata(SCHEMA_TYPE_METADATA, options)(
       target,
       key,
       descriptor,
