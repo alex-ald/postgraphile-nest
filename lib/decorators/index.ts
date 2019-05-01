dd86b6135e8c81255b38ed5f25e771ffb79181f5
Alex Aldana <alexandern.aldana@gmail.com>
2019-04-30 21:24:37 -0400

    Removed WrapResolverFilter decorator from package
    - Still under development

--- lib/decorators/index.ts
+++ lib/decorators/index.ts
@@ -2,6 +2,7 @@ export * from './add-inflector-plugin.decorator';
 export * from './change-nullability.decorator';
 export * from './extend-schema.decorator';
 export * from './process-schema-plugin.decorator';
-export * from './wrap-resolver-filter.decorator';
+// Still being developed
+// export * from './wrap-resolver-filter.decorator';
 export * from './wrap-resolver.decorator';
 export * from './schema-type.decorator';
